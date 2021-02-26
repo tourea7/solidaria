e9344e8fef058eb67de693a271bfd918e4442288
TYA <yasmineaichatoure@gmail.com>
2021-02-26 18:53:31 +0000

    2/26/2021

--- src/app/suggestride/suggestride.component.ts
+++ src/app/suggestride/suggestride.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Bou } from '../model/bou';
+
 
 @Component({
   selector: 'app-suggestride',
@@ -8,19 +8,10 @@ import { Bou } from '../model/bou';
 })
 export class SuggestrideComponent implements OnInit {
 
-  test: Bou[] = [];
   constructor() { }
 
   ngOnInit(): void {
-    this.test.push(
-      new Bou("Bassam", "Abidjan", "02/05/2021", "7.00 AM", "2000 FCFA", 2)
-    );
-    this.test.push(
-      new Bou("Yopougon", "Treichville", "02/08/2021", "9.00 AM", "1000 FCFA", 4)
-    );
-    this.test.push(
-      new Bou("Bingerville", "Treichville", "13/07/2021", "12.00 AM", "1000 FCFA", 3)
-    );
+   
   }
 
 }
